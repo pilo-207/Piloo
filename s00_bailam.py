53926f064d4d10d7a3693d7927ea45e2d1dd8b2d
pilo-207 <146462264+pilo-207@users.noreply.github.com>
2023-10-08 01:56:33 +0000

    Nop Bai

--- s00_bailam.py
+++ s00_bailam.py
@@ -1,11 +1,32 @@
 """
 --- debai 
-Viet python ham hi()
-Tra ve cau chao 'Hi'
+Viet python ham hi(name)
+Tra ve cau chao 'Hi {name}'
 
 --- input 
-(ko co)
+hi(name)
 
---- output 
-Hi!
+--- output
+Hi {name}
+
+--- ví dụ 1
+hi('AI-BTX')
+
+output
+Hi AI-BTX
+
+--- ví dụ 2
+hi('HSU')
+
+output
+Hi HSU
 """
+
+def hi(name):
+  return f'Hi {name}'
+  pass 
+  #TODO
+
+if __name__=='__main__':
+  hi('AI-BTX')  # should see Hi AI-BTX
+  hi('HSU')     # should see Hi HSU
